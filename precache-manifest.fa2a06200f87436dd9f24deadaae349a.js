a871486c0bba441767ab804bedff470118bd1c10
buddy <buddy@buddy.works>
2021-09-21 22:45:51 +0000

    Build 1

---
+++ precache-manifest.fa2a06200f87436dd9f24deadaae349a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a88a9f2f36a1286c39d8ef86614cdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e111db7e050b6964d30",
+    "url": "/static/js/2.2094e5df.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/static/js/2.2094e5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49593763c075896c7e07",
+    "url": "/static/js/main.c82a4a03.chunk.js"
+  },
+  {
+    "revision": "5e137240570c80a5f55f",
+    "url": "/static/js/runtime-main.54917bbf.js"
+  }
+]);
