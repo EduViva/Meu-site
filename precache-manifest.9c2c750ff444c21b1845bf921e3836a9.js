2bbbab743b884ae54f8fd147045b605e22946218
Eduardo <miissjackson72@gmail.com>
2022-09-21 11:21:06 -0300

    New release: 21/09

---
+++ precache-manifest.9c2c750ff444c21b1845bf921e3836a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0437bdd24947368409930c3a50ad86",
+    "url": "/Meu-site/index.html"
+  },
+  {
+    "revision": "93bfc48049482b7263dc",
+    "url": "/Meu-site/static/js/2.1cf46116.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/Meu-site/static/js/2.1cf46116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8952fe633c86262f1b79",
+    "url": "/Meu-site/static/js/main.78e55e64.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Meu-site/static/js/main.78e55e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c508e7028d210b5d1312",
+    "url": "/Meu-site/static/js/runtime-main.fec48980.js"
+  }
+]);
